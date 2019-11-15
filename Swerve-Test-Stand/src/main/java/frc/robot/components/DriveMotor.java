d2bc38051e7a8a46ced08f4926e14b56cb75fd47
rocketguy <newcomb.luke@gmail.com>
2019-11-14 21:11:18 -0500

    Wrote DriveMotor.java

--- Swerve-Test-Stand/src/main/java/frc/robot/components/DriveMotor.java
+++ Swerve-Test-Stand/src/main/java/frc/robot/components/DriveMotor.java
@@ -1,7 +1,11 @@
 package frc.robot.components;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class DriveMotor
 {
@@ -10,4 +14,37 @@ public class DriveMotor
 
     private CANPIDController sparkPID;
     private CANSparkMax sparkMotor;
+    private CANEncoder sparkEncoder;
+
+    /**
+     * 
+     * @param motorID of the Spark Max
+     */
+    public DriveMotor(int motorID)
+    {
+        sparkMotor = new CANSparkMax(motorID, MotorType.kBrushless);
+        sparkPID = sparkMotor.getPIDController();
+        sparkEncoder = sparkMotor.getEncoder();
+
+        sparkMotor.setIdleMode(IdleMode.kCoast);
+    }
+
+    /**
+     * 
+     * @param rpm desired RPMs of the motor shaft
+     */
+    public void setDesiredRPM(double rpm)
+    {
+        sparkPID.setReference(rpm, ControlType.kVelocity);
+    }
+
+    /**
+     * 
+     * @return motor shaft velocity in RPM
+     */
+    public double getCurrentRPM()
+    {
+        return sparkEncoder.getVelocity();
+    }
+
 }
