d0c7a6e880e1324b9da02e03536a2c553c5df5f5
Glenn <texasbubb@gmail.com>
2019-11-26 21:32:26 -0500

    Update TurnMotor.java
    Adding PID for turn motor IN WORK.  Not ready for primetime.

--- Swerve-Test-Stand/src/main/java/frc/robot/components/TurnMotor.java
+++ Swerve-Test-Stand/src/main/java/frc/robot/components/TurnMotor.java
@@ -18,6 +18,11 @@ public class TurnMotor
     private double currentAngle = 0.0;
     private double desiredAngle = 0.0;
     
+// heading about a unit circle in radians.
+// private static double desiredHeading;  // rotates about the Z axis [0,2PI) rad.
+// private static double currentHeading;  // rotates about the Z axis [0,2PI) rad.
+
+
     //private CANPIDController sparkPID;
     private CANSparkMax sparkMotor;
     private CANEncoder sparkEncoder;
@@ -34,8 +39,15 @@ public class TurnMotor
     private final double SetpointRange = 2 * Math.PI;
 
     private PID headingPID = null;
-    private RollingAverage averageHeading = null;
-    
+    private RollingAverage AverageAngle = null;
+
+    private final double headingThreshold = 0.05;
+    private final int headdingAverageNumberOfSamples = 10;
+
+      // Speed component for rotation about the Z axis. [-x, x]
+    private static double vTheta;
+
+
     public TurnMotor(int motorID, int motorIndex)
     {
         sparkMotor = new CANSparkMax(motorID, MotorType.kBrushed);
@@ -54,6 +66,28 @@ public class TurnMotor
         sparkMotor.setInverted(Constants.TURN_INVERT[motorIndex]);
 
         sparkMotor.setIdleMode(Constants.TURN_IDLEMODE[motorIndex]);
+
+        // create and initialize the PID for the heading
+        headingPID = new PID(propCoeff, integCoeff, diffCoeff);
+        
+        // get the initial error and put valid data in the telemetry from the imu
+        IMUAngleProcessing();
+        
+        // set initial desired heading to the current actual heading.
+        desiredAngle= currentAngle;
+        
+        // smooths out the joystick input so it doesn't slam hi/lo
+        AverageAngle = new RollingAverage(headdingAverageNumberOfSamples);
+        
+        // initially setup the PID parameters
+        headingPID.setOutputLimits(OutputLowLimit, OutputHighLimit);
+        headingPID.setMaxIOutput(MaxIOutput);
+        headingPID.setOutputRampRate(OutputRampRate);
+        headingPID.setOutputFilter(OutputFilter);
+        headingPID.setSetpointRange(SetpointRange);
+        headingPID.setContinousInputRange(2 * Math.PI);
+        headingPID.setContinous(true);  // lets PID know we are working with a continuous range [0-360)
+
     }
 
     public double getCurrentAngle()
@@ -64,6 +98,45 @@ public class TurnMotor
    
     public void setDesiredAngle(double angle)
     {
+
+        AverageAngle.add(angle);  // average in the latest input
+
+        // if the averaged stick input is greater then the headingThreshold go ahead and adjust the heading.
+        // This keeps from updating the desiredHeading value if no joystick input is being made.
+        // Otherwise, it will always drive the desiredHeading to 0 (neutral joystick position)
+
+        if (Math.abs(AverageAngle.getAverage()) > headingThreshold) {
+          desiredAngle = currentAngle - AverageAngle.getAverage();
+          // keep heading a positive angle
+          if (desiredAngle < 0) {
+            desiredAngle += (2 * Math.PI);
+          }
+        }
+
         this.desiredAngle = angle;
+        // get PID error signal to send to the motor
+        IMUAngleProcessing();
+        // send to motor
+        sparkMotor.set(vTheta);
+    }
+
+
+    // grab the imu heading and crunch out the values used for navigation and telemetry.
+    // This method produces the heading input component to the motors from the PID that holds the
+    // desired angle.  The error from the PID is sent to the motors in the vTheta variable.
+    private void IMUAngleProcessing() {
+        // desired angle in degrees +/- 0 to 180 where CCW is + and CW is -
+   //       angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS);
+        
+        // convert  imu angle range to our [0, 2PI) range
+   //       if (angles.firstAngle < 0) {
+   //           currentHeading = angles.firstAngle + 2 * Math.PI;
+   //       } else {
+   //          currentHeading = angles.firstAngle;
+   //       }
+        
+        currentAngle = //TODO Calculate the current angle from the encoder
+
+        vTheta = headingPID.getOutput(currentAngle, desiredAngle);
     }
 }
